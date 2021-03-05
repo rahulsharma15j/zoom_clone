3be4400e2a6fe4dacfc2c0cdcf895692cc9f7240
Rahul Sharma <rahulsharma15j@gmail.com>
2021-03-05 22:35:16 +0530

    css styling

--- public/script.js
+++ public/script.js
@@ -14,22 +14,32 @@ var peer = new Peer(undefined, {
 let myVideoStream;
 
 navigator.mediaDevices
-  .getUserMedia({ video: true, audio: true })
+  .getUserMedia({ video: true, audio: false })
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
   });
 
 peer.on("open", (id) => {
   socket.emit("join-room", ROOM_ID, id);
 });
 
-socket.on("user-connected", () => {
-  connectToNewUser();
-});
-
-const connectToNewUser = () => {
-  console.log("new user");
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
 };
 
 const addVideoStream = (video, stream) => {
